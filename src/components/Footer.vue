54a26d6da3528561d29a3986673f8d675d190326
acockburn <andrew@acockburn.com>
2021-04-13 08:36:56 -0400

    Adjust footer height

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,21 +1,22 @@
 <template>
   <v-footer
-    color="indigo"
-    padless
+      height="50"
+      color="indigo"
+      padless
   >
     <v-row
-      justify="center"
-      no-gutters
+        justify="center"
+        no-gutters
     >
       <v-btn
-        v-for="link in links"
-        :key="link.title"
-        :href="link.link"
-        target="_blank"
-        color="white"
-        text
-        rounded
-        class="my-2"
+          v-for="link in links"
+          :key="link.title"
+          :href="link.link"
+          target="_blank"
+          color="white"
+          text
+          rounded
+          class="my-2"
       >
         {{ link.title }}
       </v-btn>
@@ -24,14 +25,14 @@
 </template>
 
 <script>
-  export default {
-    name: 'Footer',
-    data: () => ({
-      links: [
-        {title: 'GitHub', link: 'https://github.com/AppDaemon'},
-        {title: 'Docs', link: 'https://appdaemon.readthedocs.io/en/latest/'},
-        {title: 'Forum', link: 'https://community.home-assistant.io/c/third-party/appdaemon/21'},
-      ],
-    }),
-  }
+export default {
+  name: 'Footer',
+  data: () => ({
+    links: [
+      {title: 'GitHub', link: 'https://github.com/AppDaemon'},
+      {title: 'Docs', link: 'https://appdaemon.readthedocs.io/en/latest/'},
+      {title: 'Forum', link: 'https://community.home-assistant.io/c/third-party/appdaemon/21'},
+    ],
+  }),
+}
 </script>
