c7fd8a25561d1c3bfa31f01cff2bce579948fb5a
acockburn <andrew@acockburn.com>
2021-04-14 08:56:12 -0400

    Change links to icons and add discord

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -9,16 +9,17 @@
         no-gutters
     >
       <v-btn
-          v-for="link in links"
-          :key="link.title"
-          :href="link.link"
-          target="_blank"
+          v-for="icon in icons"
+          :key="icon.name"
           color="white"
           text
           rounded
           class="my-2"
       >
-        {{ link.title }}
+
+      <a :href="icon.url" :target="icon.target">
+        <v-icon color="white" size="24px">{{ icon.name }}</v-icon>
+      </a>
       </v-btn>
     </v-row>
   </v-footer>
@@ -28,10 +29,11 @@
 export default {
   name: 'Footer',
   data: () => ({
-    links: [
-      {title: 'GitHub', link: 'https://github.com/AppDaemon'},
-      {title: 'Docs', link: 'https://appdaemon.readthedocs.io/en/latest/'},
-      {title: 'Forum', link: 'https://community.home-assistant.io/c/third-party/appdaemon/21'},
+    icons: [
+      {name: 'mdi-github', url: 'https://github.com/AppDaemon', target: "_blank"},
+      {name: 'mdi-text-box', url: 'https://appdaemon.readthedocs.io/en/latest/', target: "_blank"},
+      {name: 'mdi-discord', url: 'https://discord.gg/sgSr79jW5x', target: "_blank"},
+      {name: 'mdi-forum', url: 'https://community.home-assistant.io/c/third-party/appdaemon/21', target: "_blank"},
     ],
   }),
 }
